e66bd8d32b797de585fada07f639ce40953cc989
Omar Magdy <husseino>
2020-03-27 20:31:45 +0100

    Added mail configuration

--- config.py
+++ config.py
@@ -7,3 +7,9 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    MAIL_SERVER = "mail.fim.uni-passau.de"
+    MAIL_PORT = 25
+    MAIL_USE_TLS = 0
+    MAIL_PASSWORD=""
+    MAIL_DEFAULT_SENDER="offdet.no-reply@uni-passau.de"
+
